71286c1164791401729be33758b41ee414b4dd6a
wicthorsilva <luizwicthor@gmail.com>
2023-05-25 19:49:12 -0300

    criação de interface UserAtts

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,27 @@
 import fastify from "fastify";
-import cors from "@fastify/cors";
+// import cors from "@fastify/cors";
 import dotenv from "dotenv";
+import {PrismaClient} from "@prisma/client"
 
 dotenv.config();
 const port: any = process.env.PORT;
 
+const prisma = new PrismaClient();
+
 const server = fastify();
 
-server.register(cors, {
-    //Opções
-})
+// server.register(cors, {
+//     //Opções
+// })
 
 server.get('/', (request, reply) => {
     return "Servidor on-line...";
 });
 
+interface UserAttrs {
+    email: string,
+    password: string,
+}
 
 server.listen ({port: 3000}, (error, address) => {
     if (error) {
